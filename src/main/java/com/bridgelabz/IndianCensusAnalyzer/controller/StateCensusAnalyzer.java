ecd0e11f90a9230d2337290e9a6745a3bec68c72
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-30 02:15:23 +0530

    REFACTORED : Exception handled at main class which handled in Test file

--- src/main/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzer.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.IndianCensusAnalyzer.controller;
 
+import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
 import com.bridgelabz.IndianCensusAnalyzer.model.LoadStates;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
@@ -14,20 +17,29 @@ import java.util.stream.StreamSupport;
 public class StateCensusAnalyzer {
     /**
      * Loading data to count the number of entries
+     *
      * @param StateCsvPath
      * @return
      * @throws IOException
      */
-    public int loadStateCensusData(String StateCsvPath) throws IOException {
-        int noOfEntries;
-        try (
-                Reader reader = Files.newBufferedReader(Paths.get(StateCsvPath));
-        ) {
-            CsvToBean<LoadStates> csvToBean = new CsvToBeanBuilder(reader).withType(LoadStates.class).
-                    withIgnoreLeadingWhiteSpace(true).build();
-            Iterator<LoadStates> censusCsvIterator = csvToBean.iterator();
-            Iterable<LoadStates> csvIterable = () -> censusCsvIterator;
-            noOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+    public int loadStateCensusData(String StateCsvPath) throws CensusAnalyzerException {
+        int noOfEntries = 0;
+        try {
+            try (
+                    Reader reader = Files.newBufferedReader(Paths.get(StateCsvPath));
+            ) {
+                CsvToBean<LoadStates> csvToBean = new CsvToBeanBuilder(reader).withType(LoadStates.class).
+                        withIgnoreLeadingWhiteSpace(true).build();
+                Iterator<LoadStates> censusCsvIterator = csvToBean.iterator();
+                Iterable<LoadStates> csvIterable = () -> censusCsvIterator;
+                noOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            }
+        } catch (InvalidPathException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
+        } catch (FileNotFoundException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.WRONGEXTESNSION, "Please Enter Proper File Path");
+        } catch (IOException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
         }
         return noOfEntries;
     }
