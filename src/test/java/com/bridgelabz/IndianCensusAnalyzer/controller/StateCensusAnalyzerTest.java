e58c7b88a97978b764ab88cb31e47a569a3ccd38
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-30 02:12:02 +0530

    ADDED : File parsing Exception handled

--- src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
@@ -4,9 +4,6 @@ import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public class StateCensusAnalyzerTest {
     private static final String STATECENSUSCSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData.csv";
     private static final String STATECODECSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/" +
@@ -18,11 +15,7 @@ public class StateCensusAnalyzerTest {
     public void givenStateCensusCsvFile_LoadData_ShouldReturnNoOfEntries() throws CensusAnalyzerException {
         StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
         int checkEntries = 0;
-        try {
-            checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
-        } catch (IOException e) {
-            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
-        }
+        checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
         Assert.assertEquals(29, checkEntries);
     }
 
@@ -30,23 +23,24 @@ public class StateCensusAnalyzerTest {
     public void givenFilePath_NotCorrect_shouldHandleException() throws CensusAnalyzerException {
         StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
         int checkEntries = 0;
-        try {
-            checkEntries = stateCensus.loadStateCensusData(WRONGPATH);
-        } catch (IOException e) {
-            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
-        }
+        checkEntries = stateCensus.loadStateCensusData(WRONGPATH);
         Assert.assertEquals(29, checkEntries);
+
     }
 
     @Test
     public void givenFilePath_FileExtensionNotCorrect_shouldHandleException() throws CensusAnalyzerException {
         StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
         int checkEntries = 0;
-        try {
-            checkEntries = stateCensus.loadStateCensusData(WRONGEXTESNSION);
-        } catch (IOException e) {
-            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.WRONGEXTESNSION, "Please Enter Proper File Extension");
-        }
+        checkEntries = stateCensus.loadStateCensusData(WRONGEXTESNSION);
+        Assert.assertEquals(29, checkEntries);
+    }
+
+    @Test
+    public void givenFile_DelimiterIncorrect_shouldHandleException() throws CensusAnalyzerException {
+        StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
+        int checkEntries = 0;
+        checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
         Assert.assertEquals(29, checkEntries);
     }
 }
