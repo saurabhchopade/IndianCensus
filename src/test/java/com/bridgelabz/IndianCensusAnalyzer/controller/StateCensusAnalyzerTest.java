87a3bab052abf71773f153377b2534cca454f398
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-30 00:13:19 +0530

    ADDED : Given file path not correct should handle custom exception

--- src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
@@ -1,18 +1,38 @@
 package com.bridgelabz.IndianCensusAnalyzer.controller;
 
+import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class StateCensusAnalyzerTest {
-    private static final String STATECENSUSCSVPATH ="/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData.csv";
-    private static final String STATECODECSVPATH="/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/" +
+    private static final String STATECENSUSCSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData.csv";
+    private static final String STATECODECSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/" +
             "IndiaStateCode.csv";
+    private static final String WRONGPATH = "/home/saurabh/IdeaProjects/IndianCzer/src/test/resources/IndiaStateCensusData.csv";
+
     @Test
-    public void givenStateCensusCsvFile_LoadData_ShouldReturnNoOfEntries() throws IOException {
-        StateCensusAnalyzer stateCensus=new StateCensusAnalyzer();
-        int checkEntries=stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
-        Assert.assertEquals(29,checkEntries);
+    public void givenStateCensusCsvFile_LoadData_ShouldReturnNoOfEntries() throws CensusAnalyzerException {
+        StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
+        int checkEntries = 0;
+        try {
+            checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
+        } catch (IOException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
+        }
+        Assert.assertEquals(29, checkEntries);
+    }
+
+    @Test
+    public void givenFilePath_NotCorrect_shouldHandleException() throws CensusAnalyzerException {
+        StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
+        int checkEntries = 0;
+        try {
+            checkEntries = stateCensus.loadStateCensusData(WRONGPATH);
+        } catch (IOException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
+        }
+        Assert.assertEquals(29, checkEntries);
     }
 }
