0c180f04969da3f2e0402b385f0c44792633efc9
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-30 01:08:37 +0530

    ADDED : Exception Handled if user give wrong file type

--- src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
@@ -4,13 +4,15 @@ import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class StateCensusAnalyzerTest {
     private static final String STATECENSUSCSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData.csv";
     private static final String STATECODECSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/" +
             "IndiaStateCode.csv";
-    private static final String WRONGPATH = "/home/saurabh/IdeaProjects/IndianCzer/src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONGPATH = "/home/saurabh/IdeaProjects/IndianateCensusData.csv";
+    private static final String WRONGEXTESNSION = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData";
 
     @Test
     public void givenStateCensusCsvFile_LoadData_ShouldReturnNoOfEntries() throws CensusAnalyzerException {
@@ -32,6 +34,18 @@ public class StateCensusAnalyzerTest {
             checkEntries = stateCensus.loadStateCensusData(WRONGPATH);
         } catch (IOException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
+        }
+        Assert.assertEquals(29, checkEntries);
+    }
+
+    @Test
+    public void givenFilePath_FileExtensionNotCorrect_shouldHandleException() throws CensusAnalyzerException {
+        StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
+        int checkEntries = 0;
+        try {
+            checkEntries = stateCensus.loadStateCensusData(WRONGEXTESNSION);
+        } catch (IOException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.WRONGEXTESNSION, "Please Enter Proper File Extension");
         }
         Assert.assertEquals(29, checkEntries);
     }
