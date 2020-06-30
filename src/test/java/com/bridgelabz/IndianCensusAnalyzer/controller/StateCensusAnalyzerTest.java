5b978dc0b73a903d484eb6559aa547d2b26d048c
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-30 10:03:11 +0530

    ADDED : State census code matches or not checked using assert

--- src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/IndianCensusAnalyzer/controller/StateCensusAnalyzerTest.java
@@ -6,13 +6,11 @@ import org.junit.Test;
 
 public class StateCensusAnalyzerTest {
     private static final String STATECENSUSCSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData.csv";
-    private static final String STATECODECSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/" +
-            "IndiaStateCode.csv";
     private static final String WRONGPATH = "/home/saurabh/IdeaProjects/IndianateCensusData.csv";
     private static final String WRONGEXTESNSION = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCensusData";
-
+    private static final String STATECODECSVPATH = "/home/saurabh/IdeaProjects/IndianCensusAnalazer/src/test/resources/IndiaStateCode.csv";
     @Test
-    public void givenStateCensusCsvFile_LoadData_ShouldReturnNoOfEntries() throws CensusAnalyzerException {
+    public void givenStateCensusCsvFile_LoadData_ShouldMatchNoOFRecords() throws CensusAnalyzerException {
         StateCensusAnalyzer stateCensus = new StateCensusAnalyzer();
         int checkEntries = 0;
         checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
@@ -50,5 +48,14 @@ public class StateCensusAnalyzerTest {
         int checkEntries = 0;
         checkEntries = stateCensus.loadStateCensusData(STATECENSUSCSVPATH);
         Assert.assertEquals(29, checkEntries);
+    }
+//============================
+    @Test
+    public void givenStateCodeFile__shouldMatchNumberOfRecords() throws CensusAnalyzerException {
+
+        StateCensusCode stateCensusCode=new StateCensusCode();
+        int checkStateCodeEntries=0;
+        checkStateCodeEntries=stateCensusCode.loadStateCensusCodeData(STATECODECSVPATH);
+        Assert.assertEquals(37,checkStateCodeEntries);
     }
 }
