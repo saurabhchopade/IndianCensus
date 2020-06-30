114e8da885229c3e6af8dba89e709ad7e68c6762
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-01 00:59:10 +0530

    REFACTORED : Code and Introduced CSV Builder and Factory to ensure Programming for Interface then Implementation

--- src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/OpenCsvBuilder.java
+++ src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/OpenCsvBuilder.java
@@ -1,20 +1,8 @@
 package com.bridgelabz.IndianCensusAnalyzer.service;
 import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
 import java.io.Reader;
 import java.util.Iterator;
 
-public class OpenCsvBuilder {
-
-    public <E>Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyzerException {
-        try {
-            CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader).withType(csvClass).withIgnoreLeadingWhiteSpace(true).build();
-            return csvToBean.iterator();
-        } catch (IllegalStateException e) {
-            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEINTERNALISSUE, "Please Provide Correct Data");
-        }
-    }
+public interface OpenCsvBuilder {
+     <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyzerException;
 }
-
--- src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/StateCensusAnalyzer.java
@@ -3,8 +3,6 @@ package com.bridgelabz.IndianCensusAnalyzer.service;
 import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
 import com.bridgelabz.IndianCensusAnalyzer.model.StateCensusCodeCSV;
 import com.bridgelabz.IndianCensusAnalyzer.model.StatesCensusCSV;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,7 +18,8 @@ public class StateCensusAnalyzer {
     public int loadStateCensusData(String StateCsvPath) throws CensusAnalyzerException {
         try {
             try (Reader reader = Files.newBufferedReader(Paths.get(StateCsvPath))) {
-                Iterator<StatesCensusCSV> censusCsvIterator = this.getCSVFileIterator(reader, StatesCensusCSV.class);
+                OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
+                Iterator<StatesCensusCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StatesCensusCSV.class);
                 return this.getCount(censusCsvIterator);
             }
         } catch (FileNotFoundException e) {
@@ -35,7 +34,8 @@ public class StateCensusAnalyzer {
     public int loadStateCensusCodeData(String StateCodeCsvPath) throws CensusAnalyzerException {
         try {
             try (Reader reader = Files.newBufferedReader(Paths.get(StateCodeCsvPath))) {
-                Iterator<StateCensusCodeCSV> censusCsvIterator = this.getCSVFileIterator(reader, StateCensusCode.class);
+                OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
+                Iterator<StateCensusCodeCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StateCensusCodeCSV.class);
                 return this.getCount(censusCsvIterator);
             }
         } catch (FileNotFoundException e) {
@@ -51,16 +51,6 @@ public class StateCensusAnalyzer {
         Iterable<E> csvIterable = () -> iterator;
         int noOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
         return noOfEntries;
-    }
-
-    private <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyzerException {
-        try {
-            CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader).withType(csvClass).
-                    withIgnoreLeadingWhiteSpace(true).build();
-            return csvToBean.iterator();
-        } catch (IllegalStateException e) {
-            throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEINTERNALISSUE, "Please Provide Correct Data");
-        }
     }
 
 }
