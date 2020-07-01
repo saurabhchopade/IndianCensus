9e284ffb59034ecdb5d5c763e65e10e7a46645fc
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-01 08:45:18 +0530

    REFACTORED : Code to Introduce CSV Exception to ensure CSV Builder encapsulate

--- src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/IndianCensusAnalyzer/service/StateCensusAnalyzer.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.IndianCensusAnalyzer.service;
 
 import com.bridgelabz.IndianCensusAnalyzer.exception.CensusAnalyzerException;
+import com.bridgelabz.IndianCensusAnalyzer.exception.CsvBuilderException;
 import com.bridgelabz.IndianCensusAnalyzer.model.StateCensusCodeCSV;
 import com.bridgelabz.IndianCensusAnalyzer.model.StatesCensusCSV;
 
@@ -14,38 +15,50 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class StateCensusAnalyzer {
-
+    /**
+     * To Load The State Census Csv File
+     * @param StateCsvPath
+     * @return
+     * @throws CensusAnalyzerException
+     */
     public int loadStateCensusData(String StateCsvPath) throws CensusAnalyzerException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(StateCsvPath));
-
-                OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
-                Iterator<StatesCensusCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StatesCensusCSV.class);
-                return this.getCount(censusCsvIterator);
-            }
-         catch (FileNotFoundException e) {
+            OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
+            Iterator<StatesCensusCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StatesCensusCSV.class);
+            return this.getCount(censusCsvIterator);
+        } catch (FileNotFoundException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.WRONGEXTESNSION, "Please Enter Proper File Path");
         } catch (RuntimeException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEINTERNALISSUE, "Please check in file Content ");
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
+        } catch (CsvBuilderException e) {
+            throw new CensusAnalyzerException(e.getMessage(), e.type.name());
         }
     }
 
+    /**
+     * To Load the StateCode CSV File
+     * @param StateCodeCsvPath
+     * @return
+     * @throws CensusAnalyzerException
+     */
     public int loadStateCensusCodeData(String StateCodeCsvPath) throws CensusAnalyzerException {
         try {
-            try (Reader reader = Files.newBufferedReader(Paths.get(StateCodeCsvPath))) {
-                OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
-                Iterator<StateCensusCodeCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StateCensusCodeCSV.class);
-                return this.getCount(censusCsvIterator);
-            }
+            Reader reader = Files.newBufferedReader(Paths.get(StateCodeCsvPath));
+            OpenCsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
+            Iterator<StateCensusCodeCSV> censusCsvIterator = csvBuilder.getCSVFileIterator(reader, StateCensusCodeCSV.class);
+            return this.getCount(censusCsvIterator);
+
         } catch (FileNotFoundException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.WRONGEXTESNSION, "Please Enter Proper File Extension");
         } catch (InvalidPathException | IOException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEPATHNOTCORRECT, "Please Enter Proper File Path");
         } catch (RuntimeException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.exeptiontype.FILEINTERNALISSUE, "Please check in file Content ");
+        } catch (CsvBuilderException e) {
+            throw new CensusAnalyzerException(e.getMessage(), e.type.name());
         }
     }
 
